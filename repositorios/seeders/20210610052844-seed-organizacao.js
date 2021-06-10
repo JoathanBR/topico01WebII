a56c4263f34b503dfeb855d985ccd5c911e0501b
JoathanBR <joathanlopes@hotmail.com>
2021-06-10 09:25:35 -0300

    Organizando os dados que serão enviados na seed

--- repositorios/seeders/20210610052844-seed-organizacao.js
+++ repositorios/seeders/20210610052844-seed-organizacao.js
@@ -7,6 +7,7 @@ module.exports = {
 				duracao: 60,
 				dataLancamento: 10/05/2014,
         avaliacao: 'Excelente',
+        descricao: 'Reunião de vários heróis da Marvel',
         status: 'Disponível',
 			},
 			{
@@ -15,6 +16,7 @@ module.exports = {
 				duracao: 3600,
 				dataLancamento: 25/02/2012,
         avaliacao: 'Muto bom',
+        descricao: 'Reunião de vários heróis de cores diferentes',
         status: 'Disponível',
 			},
 			{
@@ -23,6 +25,7 @@ module.exports = {
 				duracao: 70,
 				dataLancamento: 12/11/2011,
         avaliacao: 'Bom',
+        descricao: 'Reunião de vários mutantes',
         status: 'Disponível',
 			}
 	], {})
